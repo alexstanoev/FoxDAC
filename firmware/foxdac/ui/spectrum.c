cde625aabcf5e0e0a2667a3c7fbf6fc3ba689952
Alex Stanoev <alex@astanoev.com>
2021-08-29 15:16:16 +0100

    Rebin spectrum to log axis

--- firmware/foxdac/ui/spectrum.c
+++ firmware/foxdac/ui/spectrum.c
@@ -15,50 +15,55 @@
 
 #include "kissfft/kiss_fftr.h"
 
+#define FFT_SIZE 1024
+#define FFT_SIZE_F (1024.0f)
+
+#define NUM_BARS 41
+#define NUM_BARS_F 41.0f
+
+#define MIN_BAR_FREQ 50.0f
+#define MAX_BAR_FREQ 20000.0f
+
+#define FFT_BIN_SIZE (sample_rate / FFT_SIZE)
+
 static uint8_t spectrum_running = 0;
-
-lv_obj_t * Spectrum;
-
-lv_obj_t * chart;
-lv_coord_t value_array[41];
-
-lv_coord_t target_value_array[41];
-lv_coord_t old_value_array[41];
-
-lv_timer_t * spectrum_timer;
-
-static int runs = 0;
+static uint32_t sample_rate = 48000;
 
 static int interp_step = 0;
 static const int interp_times = 3;
 
-#define FFT_SIZE 80
+static uint16_t sample_buf[FFT_SIZE];
+static volatile int sample_buf_pos = 0;
 
-uint16_t sample_buf[FFT_SIZE];
-volatile int sample_buf_pos = 0;
+static kiss_fftr_cfg fft_cfg;
+static kiss_fft_scalar fft_in[FFT_SIZE];
+static kiss_fft_cpx fft_out[FFT_SIZE];
 
-kiss_fft_scalar fft_in[FFT_SIZE];
-int fft_in_pos = 0;
+#define FFT_MEM_LEN_BYTES 16384
+static uint8_t fft_mem[FFT_MEM_LEN_BYTES];
+static size_t fft_mem_len = FFT_MEM_LEN_BYTES;
 
-kiss_fft_cpx fft_out[FFT_SIZE];
+static lv_obj_t * chart;
+static lv_coord_t value_array[NUM_BARS];
 
-#define FFT_MEM_LEN_BYTES 8192
-uint8_t fft_mem[FFT_MEM_LEN_BYTES];
-size_t fft_mem_len = FFT_MEM_LEN_BYTES;
+static lv_coord_t target_value_array[NUM_BARS];
+static lv_coord_t old_value_array[NUM_BARS];
 
-kiss_fftr_cfg fft_cfg;
+static lv_timer_t * spectrum_timer;
+
+lv_obj_t * Spectrum;
 
 static float mapRange(float a1, float a2, float b1, float b2, float s) {
-    return b1 + (s-a1)*(b2-b1)/(a2-a1);
+    return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
 }
 
 static float hanningWindow(short in, size_t i, size_t s) {
-    return in*0.5f*(1.0f-cosf(2.0f*M_PI*(float)(i)/(float)(s-1.0f)));
+    return in * 0.5f * (1.0f - cosf(2.0f * ((float)M_PI) * (float)(i) / (float)(s - 1.0f)));
 }
 
-// called from usb_spdif.c, runs in IRQ
+// called from usb_spdif.c, runs in IRQ on core 0
 void spectrum_consume_samples(int16_t* samples, uint32_t sample_count, uint32_t rate) {
-    if(!spectrum_running || sample_buf_pos == FFT_SIZE) return;
+    if(!spectrum_running || sample_buf_pos == FFT_SIZE || interp_step < interp_times) return;
 
     for (int i = 0; i < sample_count * 2; i += 2) {
         // if the sample rate is 96000, decimate by 2 to get 48000
@@ -68,6 +73,11 @@ void spectrum_consume_samples(int16_t* samples, uint32_t sample_count, uint32_t 
             if(i + 2 < sample_count * 2) {
                 i += 2;
             }
+
+            // decimated down to 48k
+            sample_rate = 48000;
+        } else {
+            sample_rate = rate;
         }
 
         // average channels
@@ -82,39 +92,47 @@ void spectrum_consume_samples(int16_t* samples, uint32_t sample_count, uint32_t 
 void spectrum_loop(void) {
     if(!spectrum_running || sample_buf_pos != FFT_SIZE || interp_step < interp_times) return;
 
-    fft_in_pos = 0;
     // apply window
     for (int i = 0; i < sample_buf_pos; i ++) {
-        fft_in[fft_in_pos++] = hanningWindow((float) sample_buf[i], i, FFT_SIZE);
+        fft_in[i] = hanningWindow((float) sample_buf[i], i, FFT_SIZE);
     }
 
-    // compute fast fourier transform
+    // compute FFT
     kiss_fftr(fft_cfg, fft_in, fft_out);
 
-    // TODO log frequency axis
-    // compute power and calculate max freq component
-    float max_power = 0;
-    int max_idx = 0;
-    for (int i = 0; i < 40; i++) {
-        float power = 20 * log10f(sqrtf(fft_out[i].r * fft_out[i].r + fft_out[i].i * fft_out[i].i) / 40.0f);
+    for (int i = 0; i < NUM_BARS; i++) {
+        // log scale, bins spaced at:
+        // min * (max/min) ^ x
+        float bar_freq = MIN_BAR_FREQ * powf((MAX_BAR_FREQ / MIN_BAR_FREQ), i / NUM_BARS_F);
+        float bar_freq_next = ((i + 1) == NUM_BARS) ? MAX_BAR_FREQ : (MIN_BAR_FREQ * powf((MAX_BAR_FREQ / MIN_BAR_FREQ), (i + 1) / NUM_BARS_F));
 
-        //power -= 20;
-        if(power < 0) power = 0;
+        int startbin = floorf(bar_freq / FFT_BIN_SIZE);
+        int endbin = ceilf(bar_freq_next / FFT_BIN_SIZE);
 
-        //printf("%d : %f %f\n", i, power, mapRange(0, 130, 0, 64, power));
+        //printf("%d %f %f %d %d\n", i, bar_freq, bar_freq_next, startbin, endbin);
 
-        // 96dB dynamic range with 16 bits
+        // resolution at the first bars is too low, just draw some bins directly
+        if(startbin == 1 && endbin == 2) {
+            startbin = i;
+            endbin = i + 1;
+        }
+
+        // rebin
+        float power_sum = 0;
+        for(int j = startbin; j < endbin; j++) {
+            power_sum += sqrtf(fft_out[j].r * fft_out[j].r + fft_out[j].i * fft_out[j].i) / FFT_SIZE_F;
+        }
+
+        power_sum = 20 * log10f(power_sum / (endbin - startbin));
+
+        //float power = 20 * log10f(sqrtf(fft_out[i].r * fft_out[i].r + fft_out[i].i * fft_out[i].i) / FFT_SIZE_F);
+
+        // 96dB dynamic range with 16 bits + fudge window loss
         old_value_array[i] = target_value_array[i];
-        target_value_array[i] = mapRange(0, 96, 0, 64, power);
+        target_value_array[i] = mapRange(0, 90, 0, 64, power_sum);
     }
 
-    // empty bar with size 80 FFT
-    old_value_array[41] = 0;
-    target_value_array[41] = 0;
-
     interp_step = 0;
-
-    fft_in_pos = 0;
     sample_buf_pos = 0;
 }
 
@@ -124,7 +142,7 @@ static void redraw_bars(lv_timer_t * timer) {
         float fac = ((float) interp_step) / ((float) interp_times);
         float lerp = fac * fac * (3.0f - 2.0f * fac);
 
-        for(int i = 0; i < 41; i++) {
+        for(int i = 0; i < NUM_BARS; i++) {
             float oldval = ((float) old_value_array[i]);
             float newval = ((float) target_value_array[i]);
 
@@ -168,14 +186,14 @@ void spectrum_init(void) {
 
     lv_coord_t * ser_array =  lv_chart_get_y_array(chart, ser);
 
-    for(int i = 0; i < 41; i++) {
+    for(int i = 0; i < NUM_BARS; i++) {
         ser_array[i] = 0;
         target_value_array[i] = 0;
     }
 
     lv_chart_refresh(chart);
 
-    spectrum_timer = lv_timer_create(redraw_bars, 30, NULL);
+    spectrum_timer = lv_timer_create(redraw_bars, 20, NULL);
     lv_timer_pause(spectrum_timer);
 }
 
